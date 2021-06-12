28b95a2999039bc42b023e9124a3def1caa3ee2c
pedroaugusto90 <pedro.aquino@etec.sp.gov.br>
2021-06-11 22:12:01 -0300

    Adicionar arquivos de projeto.

---
+++ CadProduto/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CadProduto
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CadProduto/Models/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using System.Linq;
+using System.Web;
+
+namespace CadProduto.Models
+{
+    public class Produto
+    {
+        [Required(ErrorMessage = "Insira o código!")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Preencha o nome do produto!")]
+        public string Nome { get; set; }
+        
+        [Required]
+        public string Descricao { get; set; }
+
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Valor inválido!")]
+        public string Peso { get; set; }
+        
+        [Required(ErrorMessage = "Por favor, insira a categoria do produto!")]
+        public string Categoria { get; set; }
+        
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$",ErrorMessage = "Preço único inválido!")]
+        public string precoUnico { get; set; }
+    }
+}
